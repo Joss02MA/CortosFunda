2edd34627ce6d6068283951a2802f436d2e05393
00155020 <00155020@uca.edu.sv>
2020-06-26 17:14:40 -0600

    notas

---
+++ Corto4/Ejercicio3.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+void llenarMatriz (float arreglo[])
+{
+    cout << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Ingrese notas: "; cin >> arreglo[i];
+    }
+    cout << endl;
+}
+
+float resultadoFinal (float arreglo[])
+{
+    float suma = 0, notaFinal;
+
+    for (int i = 0; i < 5; i++)
+    {
+        suma += arreglo[i];
+    }
+    notaFinal = suma/5;
+    cout << "La nota final es: " << notaFinal << endl;
+    
+    if (notaFinal >= 6)
+    {
+        cout << endl;
+        cout << "APROBADO" << endl;
+        cout << endl;
+    }
+    else
+    {
+        cout << endl;
+        cout << "REPROBADO" << endl;
+        cout << endl;
+    }
+}
+
+int main ()
+{
+    int estudiantes;
+    float arreglo[5];
+    float resultado[5];
+
+    cout << endl;
+    cout << "Ingrese el numero de estudiantes a evaluar: ";
+    cin >> estudiantes;
+    
+    for (int i = 0; i < estudiantes; i++)
+    {
+        llenarMatriz (arreglo);
+        resultadoFinal (arreglo);
+    }
+    
+    return 0;
+}
